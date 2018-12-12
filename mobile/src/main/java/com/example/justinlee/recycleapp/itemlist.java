c3660d0b1f3bd3a3c9e3b626044ee9a404f68d2a
ahamliu <ahamliu@umass.edu>
2018-12-12 15:33:34 -0500

    item list category buttons now redirect

--- mobile/src/main/java/com/example/justinlee/recycleapp/itemlist.java
+++ mobile/src/main/java/com/example/justinlee/recycleapp/itemlist.java
@@ -10,6 +10,10 @@ import android.view.View;
 public class itemlist extends AppCompatActivity {
 
     ImageButton backButton;
+    carbon.widget.Button plasticButton;
+    carbon.widget.Button paperButton;
+    carbon.widget.Button glassButton;
+    carbon.widget.Button electronicsButton;
     carbon.widget.FloatingActionButton plusButton;
 
     @Override
@@ -24,6 +28,43 @@ public class itemlist extends AppCompatActivity {
                 startActivity(intentLoadActivity);
             }
         });
+        //plastic guide page
+        plasticButton = (carbon.widget.Button) findViewById(R.id.plastic_button) ;
+        plasticButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadActivity = new Intent(itemlist.this, plastic.class);
+                startActivity(intentLoadActivity);
+            }
+        });
+        //paper guide page
+        paperButton = (carbon.widget.Button) findViewById(R.id.paper_button) ;
+        paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadActivity = new Intent(itemlist.this, paper.class);
+                startActivity(intentLoadActivity);
+            }
+        });
+        //glass guide page
+        glassButton = (carbon.widget.Button) findViewById(R.id.glass_button) ;
+        glassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadActivity = new Intent(itemlist.this, glass.class);
+                startActivity(intentLoadActivity);
+            }
+        });
+        //electronics guide page
+        electronicsButton = (carbon.widget.Button) findViewById(R.id.electronics_button) ;
+        electronicsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadActivity = new Intent(itemlist.this, electronics.class);
+                startActivity(intentLoadActivity);
+            }
+        });
+
 
         plusButton = (carbon.widget.FloatingActionButton) findViewById(R.id.plus_button) ;
 
---
+++ mobile/src/main/java/com/example/justinlee/recycleapp/paper.java
@@ -0,0 +1,13 @@
+package com.example.justinlee.recycleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class paper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paper);
+    }
+}
