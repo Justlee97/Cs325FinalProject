a897c3d97ff3fdafbaf4ca045667267ece7dee2e
ahamliu <ahamliu@umass.edu>
2018-12-12 16:30:30 -0500

    guide pages

--- mobile/src/main/java/com/example/justinlee/recycleapp/paper.java
+++ mobile/src/main/java/com/example/justinlee/recycleapp/paper.java
@@ -1,13 +1,26 @@
 package com.example.justinlee.recycleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class paper extends AppCompatActivity {
+
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paper);
+        backButton = (ImageButton) findViewById(R.id.back_button) ;
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadActivity = new Intent(paper.this, itemlist.class);
+                startActivity(intentLoadActivity);
+            }
+        });
     }
 }
