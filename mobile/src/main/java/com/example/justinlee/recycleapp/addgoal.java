a492c28f16527dd723a60895105adbbfe1a76ef7
Fractalus <ashley.l.ta@gmail.com>
2018-12-13 02:20:40 -0500

    just add layout to add goals for now

--- mobile/src/main/java/com/example/justinlee/recycleapp/addgoal.java
+++ mobile/src/main/java/com/example/justinlee/recycleapp/addgoal.java
@@ -1,13 +1,48 @@
 package com.example.justinlee.recycleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class addgoal extends AppCompatActivity {
+
+    private Spinner goalSpinner;
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addgoal);
+
+        backButton = (ImageButton) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadActivity = new Intent(addgoal.this, itemlist.class);
+                startActivity(intentLoadActivity);
+                finish();
+            }
+        });
+
+        goalSpinner = (Spinner) findViewById(R.id.spinner);
+        String[] categoriesSpinner = new String[]{"Category", "Any", "Plastic", "Glass", "Paper", "Electronics"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(addgoal.this, R.layout.spinner_item, categoriesSpinner) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        };
+        goalSpinner.setAdapter(adapter);
     }
 }
