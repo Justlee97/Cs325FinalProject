69fe7ce1b8fdea964d7d9647f27aa963fab242c0
Fractalus <ashley.l.ta@gmail.com>
2018-12-13 14:43:08 -0500

    dialog classes added for profile name change

---
+++ mobile/src/main/java/com/example/justinlee/recycleapp/NewDialog.java
@@ -0,0 +1,62 @@
+package com.example.justinlee.recycleapp;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+public class NewDialog extends AppCompatDialogFragment {
+
+    private EditText editProfileName;
+    private DialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_layout, null);
+
+        builder.setView(view)
+                .setTitle("Set Profile Name")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        String name = editProfileName.getText().toString();
+                        listener.applyText(name);
+                    }
+                });
+
+        editProfileName = view.findViewById(R.id.edit_name);
+
+        return builder.create();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (DialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement DialogListener");
+
+        }
+    }
+    public interface DialogListener {
+        void applyText(String name);
+    }
+}
+
--- mobile/src/main/java/com/example/justinlee/recycleapp/profile.java
+++ mobile/src/main/java/com/example/justinlee/recycleapp/profile.java
@@ -1,5 +1,6 @@
 package com.example.justinlee.recycleapp;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -9,18 +10,24 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import java.lang.reflect.Type;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
-public class profile extends AppCompatActivity {
+public class profile extends AppCompatActivity implements NewDialog.DialogListener {
 
     ArrayList<String> goalArray;
     EditText userInput;
     ListView showGoal;
+
+    private TextView profileName;
+
     ImageButton backButton;
     ImageButton editButton;
     carbon.widget.FloatingActionButton plusButton;
@@ -31,62 +38,64 @@ public class profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         loadData();
 
-        //store user input into a list, and then display it with most recent on top.
-        userInput = (EditText)findViewById(R.id.goalname);
-        showGoal = (ListView)findViewById(R.id.goallist);
+        profileName = findViewById(R.id.profile_name);
 
-        plusButton = (carbon.widget.FloatingActionButton) findViewById(R.id.plus_button) ;
+        //store user input into a list, and then display it with most recent on top.
+        userInput = (EditText) findViewById(R.id.goalname);
+        showGoal = (ListView) findViewById(R.id.goallist);
+
+        plusButton = (carbon.widget.FloatingActionButton) findViewById(R.id.plus_button);
         plusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String getInput = userInput.getText().toString();
-                if(goalArray.contains(getInput)) {
+                if (goalArray.contains(getInput)) {
                     Toast.makeText(getBaseContext(), "You already added this Goal!", Toast.LENGTH_LONG);
-                }
-                else if(getInput == null || getInput.trim().equals("")) {
+                } else if (getInput == null || getInput.trim().equals("")) {
+                    //I have no idea what Toast does tbh.
                     Toast.makeText(getBaseContext(), "Enter the name of your Goal", Toast.LENGTH_LONG);
-                }
-                else{
+                } else {
                     goalArray.add(getInput);
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(profile.this, android.R.layout.simple_list_item_1, goalArray);
                     showGoal.setAdapter(adapter);
-                    ((EditText)findViewById(R.id.goalname)).setText(" ");
+                    ((EditText) findViewById(R.id.goalname)).setText(" ");
                 }
                 saveData();
             }
         });
 
-        backButton = (ImageButton) findViewById(R.id.back_button) ;
-
-            backButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intentLoadActivity = new Intent(profile.this, MainActivity.class);
-                    startActivity(intentLoadActivity);
-                }
-            });
-
-            editButton = (ImageButton) findViewById(R.id.edit_button) ;
-
-            editButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intentLoadActivity = new Intent(profile.this, MainActivity.class); //NEED TO CHANGE REDIRECTION  OF EDIT BUTTON TO EDIT PAGE (DO WE NEED AN EDIT PAGE?)
-                    startActivity(intentLoadActivity);
-                }
-            });
-
-            /*
-        plusButton = (carbon.widget.FloatingActionButton) findViewById(R.id.plus_button) ;
-
-        plusButton.setOnClickListener(new View.OnClickListener() {
+        backButton = (ImageButton) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentLoadActivity = new Intent(profile.this, addgoal.class);
+                Intent intentLoadActivity = new Intent(profile.this, MainActivity.class);
                 startActivity(intentLoadActivity);
             }
-        }); */
+        });
+
+        editButton = (ImageButton) findViewById(R.id.edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+                //Intent intentLoadActivity = new Intent(profile.this, MainActivity.class); //NEED TO CHANGE REDIRECTION  OF EDIT BUTTON TO EDIT PAGE (DO WE NEED AN EDIT PAGE?)
+                //startActivity(intentLoadActivity);
+            }
+        });
     }
+
+    public void openDialog() {
+        NewDialog exampleDialog = new NewDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    //method that will change the profile name on profile page from the dialog popup
+    @Override
+    public void applyText(String name) {
+        profileName.setText(name);
+    }
+
+    //save and load the goals whenever the app is used.
     private void saveData() {
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -100,10 +109,11 @@ public class profile extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
         Gson gson = new Gson();
         String json = sharedPreferences.getString("goal list", null);
-        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        Type type = new TypeToken<ArrayList<String>>() {
+        }.getType();
         goalArray = gson.fromJson(json, type);
 
-        if(goalArray == null) {
+        if (goalArray == null) {
             goalArray = new ArrayList<>();
         }
     }
