4312729416d54704dc2deaf109780af52758ecab
Fractalus <ashley.l.ta@gmail.com>
2018-12-13 03:25:19 -0500

    Added unsavable personal goal list onto the profile page directly.

--- mobile/src/main/java/com/example/justinlee/recycleapp/addgoal.java
+++ mobile/src/main/java/com/example/justinlee/recycleapp/addgoal.java
@@ -5,19 +5,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.ArrayList;
 
+//THIS PAGE IS DEFUNCT FOR NOW
 public class addgoal extends AppCompatActivity {
 
     private Spinner goalSpinner;
     ImageButton backButton;
+    Button addGoal;
+    ArrayList<String> addArray = new ArrayList<String>();
+    EditText userInput;
+    ListView showGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addgoal);
+
+        userInput = (EditText)findViewById(R.id.goalname);
+        showGoal = (ListView)findViewById(R.id.goallist);
+
+        addGoal = (Button)findViewById(R.id.addbutton);
+        addGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getInput = userInput.getText().toString();
+                if(addArray.contains(getInput)) {
+                    Toast.makeText(getBaseContext(), "You already added this Goal!", Toast.LENGTH_LONG);
+                }
+                else if(getInput == null || getInput.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "Enter the name of your Goal", Toast.LENGTH_LONG);
+                }
+                else{
+                    addArray.add(getInput);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(addgoal.this, android.R.layout.simple_list_item_1, addArray);
+                    showGoal.setAdapter(adapter);
+                    ((EditText)findViewById(R.id.goalname)).setText(" ");
+                }
+
+            }
+        });
 
         backButton = (ImageButton) findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
--- mobile/src/main/java/com/example/justinlee/recycleapp/profile.java
+++ mobile/src/main/java/com/example/justinlee/recycleapp/profile.java
@@ -4,10 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class profile extends AppCompatActivity {
 
+    ArrayList<String> addArray = new ArrayList<String>();
+    EditText userInput;
+    ListView showGoal;
     ImageButton backButton;
     ImageButton editButton;
     carbon.widget.FloatingActionButton plusButton;
@@ -17,7 +27,30 @@ public class profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-            backButton = (ImageButton) findViewById(R.id.back_button) ;
+        userInput = (EditText)findViewById(R.id.goalname);
+        showGoal = (ListView)findViewById(R.id.goallist);
+
+        plusButton = (carbon.widget.FloatingActionButton) findViewById(R.id.plus_button) ;
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getInput = userInput.getText().toString();
+                if(addArray.contains(getInput)) {
+                    Toast.makeText(getBaseContext(), "You already added this Goal!", Toast.LENGTH_LONG);
+                }
+                else if(getInput == null || getInput.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "Enter the name of your Goal", Toast.LENGTH_LONG);
+                }
+                else{
+                    addArray.add(getInput);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(profile.this, android.R.layout.simple_list_item_1, addArray);
+                    showGoal.setAdapter(adapter);
+                    ((EditText)findViewById(R.id.goalname)).setText(" ");
+                }
+            }
+        });
+
+        backButton = (ImageButton) findViewById(R.id.back_button) ;
 
             backButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -37,6 +70,7 @@ public class profile extends AppCompatActivity {
                 }
             });
 
+            /*
         plusButton = (carbon.widget.FloatingActionButton) findViewById(R.id.plus_button) ;
 
         plusButton.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +79,6 @@ public class profile extends AppCompatActivity {
                 Intent intentLoadActivity = new Intent(profile.this, addgoal.class);
                 startActivity(intentLoadActivity);
             }
-        });
+        }); */
     }
 }
